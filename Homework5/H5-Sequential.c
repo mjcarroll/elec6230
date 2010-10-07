5133e204c9327364ccebec1ef923991334394c21
Michael Carroll <carroll.michael@gmail.com>
2010-10-07 15:21:18 -0500

    Finished the report for Homework 5

--- Homework5/H5-Sequential.c
+++ Homework5/H5-Sequential.c
@@ -33,19 +33,16 @@ for (i=0;i<matrixSize;i++)          // Row
     for (j=0;j<matrixSize;j++)      // Column
         for (k=0;k<matrixSize;k++)
             c[i][j] += a[i][k] * b[k][j];
-
 gettimeofday(&t2,0);
 /*** Error calculation ***/
 for (i=0;i<matrixSize;i++)
     for (j=0;j<matrixSize;j++)
         error += abs(c[i][j]-(i+1.0)*(j+1.0));
-
 gettimeofday(&t3,0);
 
 float totalInt = t3.tv_sec - t0.tv_sec + (t3.tv_usec - t0.tv_usec)*1.0E-06;
 float setupInt = t1.tv_sec - t0.tv_sec + (t1.tv_usec - t0.tv_usec)*1.0E-06;
 float multInt = t2.tv_sec - t1.tv_sec + (t2.tv_usec - t1.tv_usec)*1.0E-06;
-
 printf("Total Execution Time:\t%e\n",totalInt);
 printf("Setup Time:\t%e\n",setupInt);
 printf("Multiplication Time:\t%e\n",multInt);
